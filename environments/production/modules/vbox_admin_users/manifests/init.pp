322445e9e396d6dc79c781acd1130d05809aad8f
Jason A. Fager <jafager@jafager.com>
2019-02-08 10:15:47 -0500

    Fixed typos

--- environments/production/modules/vbox_admin_users/manifests/init.pp
+++ environments/production/modules/vbox_admin_users/manifests/init.pp
@@ -51,7 +51,7 @@ class vbox_admin_users
 
     ssh_authorized_key { 'jafager':
         ensure => present,
-        user => 'jfager',
+        user => 'jafager',
         type => 'ssh-rsa',
         key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBWghKioWJSTqcCfNonD8S146NK9f8cprpWGYphxr/08Uv6u/Qlei80OQsVJKy0KKowcI482/0HkHmjbcboEZM3VZvKDlpv3lt3I13rsa0o8LrltqURSoemZzSLXTw04jwpbf3KEkcoAJhczrbUwMCKlsC1PsPXJ1PjeA9cSFLcT2zJjY+Wdy4wxkyN4K1fyXtkasAEbHHrZ3u/qTZRY/ig8/oI+il9sXOZVxbieCLzJL/8tKEgaTtI9XZA7lHY+DLD1tta9+GigvCPRSYdobLnDUi2ub4eMZIuPoLg4qBA2PqazlFStZ52pHOv106V4x7mD0+fmE5ummGORw4VzE/',
     }
