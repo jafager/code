7fedbea4adf75b654813a9f3dd1a7a027172e3f6
Jason A. Fager <jafager@jafager.com>
2019-02-09 14:32:08 -0500

    Fixed typos

--- environments/production/modules/vbox_admin_user/manifests/init.pp
+++ environments/production/modules/vbox_admin_user/manifests/init.pp
@@ -1,19 +1,6 @@
 class vbox_admin_user (String $username, String $fullname, Integer $uid, String $sshkey)
 {
 
-    group { 'admins':
-        ensure => present,
-        gid => 2100,
-    }
-
-    file { '/etc/sudoers.d/admins':
-        ensure => present,
-        owner => root,
-        group => root,
-        mode => '0600',
-        content => '%admins ALL=(ALL) NOPASSWD: ALL',
-    }
-
     group { "${username}":
         ensure => present,
         gid => $uid,
@@ -26,7 +13,7 @@ class vbox_admin_user (String $username, String $fullname, Integer $uid, String 
         groups => [
             'admins',
         ],
-        comment => "${fullname}",
+        comment => $fullname,
         home => "/home/${username}",
         shell => '/bin/bash',
         purge_ssh_keys => true,
@@ -43,8 +30,8 @@ class vbox_admin_user (String $username, String $fullname, Integer $uid, String 
 
     file { "/home/${username}/.ssh":
         ensure => directory,
-        owner => ${username},
-        group => ${username},
+        owner => $username,
+        group => $username,
         mode => '0700',
         require => File["/home/${username}"],
     }
@@ -59,9 +46,9 @@ class vbox_admin_user (String $username, String $fullname, Integer $uid, String 
 
     ssh_authorized_key { "${username}":
         ensure => present,
-        user => "${username}",
+        user => $username,
         type => 'ssh-rsa',
-        key => "${sshkey}",
+        key => "$sshkey,
         require => File["/home/${username}/.ssh"],
     }
 
