81d7c483f24a581304c5820c24ee3183bd79c04e
Jason A. Fager <jafager@jafager.com>
2019-02-10 12:08:05 -0500

    Added xinetd service and TFTP configuration

--- environments/production/modules/vbox_tftp_server/manifests/init.pp
+++ environments/production/modules/vbox_tftp_server/manifests/init.pp
@@ -10,4 +10,25 @@ class vbox_tftp_server
         require => Package['tftp-server'],
     }
 
+    package { 'xinetd':
+        ensure => present,
+    }
+
+    file { '/etc/xinetd.d/tftp':
+        ensure => present,
+        owner => root,
+        group => root,
+        mode => '0644',
+        source => 'puppet:///modules/vbox_tftp_server/etc_xinetd.d_tftp',
+        require => [
+            Package['xinetd'],
+            Package['tftp-server'],
+        ],
+    }
+
+    service { 'xinetd':
+        ensure => running,
+        subscribe => File['/etc/xinetd.d/tftp'],
+    }
+
 }
