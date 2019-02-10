2f0faa10660c379ff1f51a1ece6aadbd10af24dd
Jason A. Fager <jafager@jafager.com>
2019-02-10 11:25:30 -0500

    Reverted some changes and put all user stuff in vbox_administrators

--- environments/production/modules/vbox_administrators/manifests/init.pp
+++ environments/production/modules/vbox_administrators/manifests/init.pp
@@ -1,16 +1,79 @@
 class vbox_administrators
 {
 
-    class { 'vbox_sudo_group':
-        groupname => 'admins',
+    group { 'admins':
+        ensure => present,
         gid => 2100,
     }
 
-    class { 'vbox_admin_user':
-        username => 'jafager',
-        fullname => 'Jason A. Fager',
+    file { '/etc/sudoers.d/admins':
+        ensure => present,
+        owner => root,
+        group => root,
+        mode => '0600',
+        content => '%admins ALL=(ALL) NOPASSWD: ALL',
+    }
+
+    group { 'jafager':
+        ensure => present,
+        gid => 1100,
+    }
+
+    user { 'jafager':
+        ensure => present,
         uid => 1100,
-        sshkey => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBWghKioWJSTqcCfNonD8S146NK9f8cprpWGYphxr/08Uv6u/Qlei80OQsVJKy0KKowcI482/0HkHmjbcboEZM3VZvKDlpv3lt3I13rsa0o8LrltqURSoemZzSLXTw04jwpbf3KEkcoAJhczrbUwMCKlsC1PsPXJ1PjeA9cSFLcT2zJjY+Wdy4wxkyN4K1fyXtkasAEbHHrZ3u/qTZRY/ig8/oI+il9sXOZVxbieCLzJL/8tKEgaTtI9XZA7lHY+DLD1tta9+GigvCPRSYdobLnDUi2ub4eMZIuPoLg4qBA2PqazlFStZ52pHOv106V4x7mD0+fmE5ummGORw4VzE/',
+        gid => jafager,
+        groups => [
+            'admins',
+        ],
+        comment => 'Jason A. Fager',
+        home => '/home/jafager',
+        shell => '/bin/bash',
+        purge_ssh_keys => true,
+    }
+
+    file { '/home/jafager':
+        ensure => directory,
+        owner => jafager
+        group => jafager,
+        mode => '0700',
+        require => [
+            User['jafager'],
+            Group['jafager'],
+        ],
+    }
+
+    file { '/home/jafager/.ssh':
+        ensure => directory,
+        owner => jafager
+        group => jafager,
+        mode => '0700',
+        require => File['/home/jafager'],
+    }
+
+    file { '/home/jafager/.ssh/authorized_keys':
+        ensure => directory,
+        owner => jafager
+        group => jafager,
+        mode => '0700',
+        require => File['/home/jafager/.ssh'],
+    }
+
+    ssh_authorized_key { 'jafager':
+        ensure => present,
+        user => 'jafager',
+        type => 'ssh-rsa',
+        key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBWghKioWJSTqcCfNonD8S146NK9f8cprpWGYphxr/08Uv6u/Qlei80OQsVJKy0KKowcI482/0HkHmjbcboEZM3VZvKDlpv3lt3I13rsa0o8LrltqURSoemZzSLXTw04jwpbf3KEkcoAJhczrbUwMCKlsC1PsPXJ1PjeA9cSFLcT2zJjY+Wdy4wxkyN4K1fyXtkasAEbHHrZ3u/qTZRY/ig8/oI+il9sXOZVxbieCLzJL/8tKEgaTtI9XZA7lHY+DLD1tta9+GigvCPRSYdobLnDUi2ub4eMZIuPoLg4qBA2PqazlFStZ52pHOv106V4x7mD0+fmE5ummGORw4VzE/',
+        require => File['/home/jafager/.ssh/authorized_keys'],
+    }
+
+    file { '/home/jafager/.hushlogin":
+        ensure => present,
+        owner => jafager,
+        group => jafager,
+        mode => '0600',
+        content => '',
+        require => File['/home/jafager'],
     }
 
 }
