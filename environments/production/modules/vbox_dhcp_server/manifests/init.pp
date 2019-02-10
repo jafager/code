203eb0dbd2b51aecb0ff0478729f6daf12363de7
Jason A. Fager <jafager@jafager.com>
2019-02-10 11:52:56 -0500

    Added dhcpd.conf

--- environments/production/modules/vbox_dhcp_server/manifests/init.pp
+++ environments/production/modules/vbox_dhcp_server/manifests/init.pp
@@ -5,4 +5,14 @@ class vbox_dhcp_server
         ensure => present,
     }
 
+    file { '/etc/dhcp/dhcpd.conf':
+    {
+        ensure => present,
+        owner => root,
+        group => root,
+        mode => '0644',
+        file => 'puppet:///modules/vbox_dhcp_server/etc_dhcp_dhcpd.conf',
+        require => Package['dhcp'],
+    }
+
 }
